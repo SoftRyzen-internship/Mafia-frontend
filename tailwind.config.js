9432337924e0bac63bd5158fd5030b760b0f6287
Mariia Kovalova <mariia_kovalova@ukr.net>
2023-09-06 15:03:54 +0300

    update: tailwind config add box shadow vars

--- tailwind.config.js
+++ tailwind.config.js
@@ -38,8 +38,12 @@ module.exports = {
         grad_400:
           'linear-gradient(90deg, rgba(141, 11, 147, 0.33) 0%, rgba(255, 5, 124, 0) 100%)',
         grad_500:
-          'linear-gradient(270deg, rgba(117, 103, 140, 0.53) 0%, rgba(117, 103, 140, 0) 100%)',
+          'linear-gradient(90deg, #FF057C 0%, #8D0B93 50%, rgba(50, 21, 117, 0.00) 100%)',
         grad_600:
+          'linear-gradient(90deg, rgba(255, 5, 124, 0.35) 0%, rgba(141, 11, 147, 0.35) 50%, rgba(50, 21, 117, 0.35) 100%)',
+        grad_700:
+          'linear-gradient(270deg, rgba(117, 103, 140, 0.53) 0%, rgba(117, 103, 140, 0) 100%)',
+        grad_800:
           'linear-gradient(270deg, rgba(22, 20, 28, 0) 0%, #16141c 100%)',
       },
       fontFamily: {
@@ -50,6 +54,14 @@ module.exports = {
       // BORDER
       borderRadius: {
         normal: '6px',
+      },
+      // SHADOW
+      boxShadow: {
+        xxs: '3px 3px 4px 0px rgba(19,8,19,0.16)',
+        xs: '-2px 0px 8px 0px rgba(172, 136, 201, 0.13)',
+        sm: '-2px 0px 12px 0px rgba(62, 50, 95, 0.12)',
+        md: '0px 0px 10px rgba(0, 0, 0, 0.1)',
+        lg: '0px 0px 14px 0px rgba(159, 127, 199, 0.11)',
       },
       // ALL COLORS
       colors: {
