fbea6fae97a718c09a3a369896620a546404b38f
veronikanos <vveronikanika@gmail.com>
2023-09-08 13:30:04 +0300

    feat: add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,6 +28,31 @@ export default function RootLayout({
 }) {
   return (
     <html>
+      <head>
+        <link rel="shortcut icon" href="/images/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/images/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/images/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/images/favicon-16x16.png"
+        />
+        <link
+          rel="mask-icon"
+          href="/images/safari-pinned-tab.svg"
+          color="#5bbad5"
+        />
+      </head>
       <body className={`${montserrat.variable} ${raleway.variable}`}>
         {children}
       </body>
