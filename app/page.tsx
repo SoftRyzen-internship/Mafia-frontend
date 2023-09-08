cd6e8295f36b2019ce094b6020d51741bc150221
Volodymyr-Bezyk <99117313+Volodymyr-Bezyk@users.noreply.github.com>
2023-09-08 10:25:45 +0300

    delete import in main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import Title from '@/components/Title/Title';
 
 import data from '@/data/common.json';
-import { Schedule } from '@/components/Schedule';
 
 const Home = () => {
   const mainPage = data.mainPage;
@@ -9,7 +8,6 @@ const Home = () => {
     <>
       <Title>{mainPage.title}</Title>
       <p className="font-montserrat">{mainPage.description}</p>
-      <Schedule />
     </>
   );
 };
