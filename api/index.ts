c2b6ff29e170aaa1686d33d81ecf328c5f52c2e7
Mariia Kovalova <mariia_kovalova@ukr.net>
2023-09-11 16:45:37 +0300

    api refactor

--- api/index.ts
+++ api/index.ts
@@ -1,18 +1,2 @@
-import { gql } from 'graphql-request';
-
-export const GET_SCHEDULE = gql`
-  query {
-    schedule {
-      data {
-        attributes {
-          schedules {
-            id
-            day
-            time_start
-            time_end
-          }
-        }
-      }
-    }
-  }
-`;
+export * from './getPrices';
+export * from './getSchedule';
