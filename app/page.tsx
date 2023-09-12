11df45a6838e859cd9f3b0b3f78618990d79bfce
Volodymyr Bondarenko <qweeqer2012@gmail.com>
2023-09-12 19:18:53 +0300

    fix code after codereview/change images bg/add in data object with svg icons

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import Title from '@/components/Title/Title';
 
 import data from '@/data/common.json';
-import { TournamentsSection } from '@/views/TournamentsSection';
 
 const Home = () => {
   const mainPage = data.mainPage;
@@ -10,7 +9,6 @@ const Home = () => {
     <>
       <Title>{mainPage.title}</Title>
       <p className="font-montserrat">{mainPage.description}</p>
-      <TournamentsSection />
     </>
   );
 };
