f0ef2173ddb7dd9f8d3a275220eee4d86abf7b4a
Volodymyr Bondarenko <qweeqer2012@gmail.com>
2023-09-12 12:14:21 +0300

    adjustment of cup name styles

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-import HallFameList from '@/components/HallFameList/HallFameList';
 import Title from '@/components/Title/Title';
 
 import data from '@/data/common.json';
@@ -9,7 +8,6 @@ const Home = () => {
     <>
       <Title>{mainPage.title}</Title>
       <p className="font-montserrat">{mainPage.description}</p>
-      <HallFameList />
     </>
   );
 };
