5c6ca3bfa8ea4827849f59592b4f00b96ec0c7cb
i-havr <i.havrylov@gmail.com>
2023-09-12 15:47:07 +0300

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,9 @@
 import { CorporateParties } from '@/views/CorporateParties';
 
-import { Slider } from '@/components/Slider';
-import { SliderSchoolElement } from '@/components/SliderSchoolElement';
-import schoolData from '@/data/schoolSection.json';
-
 const Home = async () => {
   return (
     <>
       <CorporateParties />
-      <Slider
-        section="school"
-        pagination
-        navigation
-        data={schoolData.swiperData}
-        element={SliderSchoolElement}
-      />
     </>
   );
 };
