8bbb5b186e89d075814741ae1cce36acbe372882
unknown <tana.palam@gmail.com>
2023-09-13 16:25:12 +0300

    fix tournaments

--- app/tournaments/page.tsx
+++ app/tournaments/page.tsx
@@ -1,5 +1,11 @@
+import { TournamentsSection } from '@/views/TournamentsSection';
 const Tournaments = () => {
-  return <div>Tournaments page</div>;
+  return (
+    <div>
+      {/* Tournaments page */}
+      <TournamentsSection />
+    </div>
+  );
 };
 
 export default Tournaments;
