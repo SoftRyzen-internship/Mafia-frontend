64a85d6531c4b609e66f203588f4df2809c0b15c
andrey-lawyer <andrey-1@ukr.net>
2023-09-13 12:19:45 +0300

    fix color

--- components/ModalSendForm/ModalSendForm.tsx
+++ components/ModalSendForm/ModalSendForm.tsx
@@ -13,7 +13,7 @@ export const ModalSendForm = ({
   return (
     <div className=" mx-auto max-w-[480px] px-6  md:mx-0 md:w-auto md:px-0   ">
       <div
-        className={`${s.background} relative  z-40 mt-[100px] rounded-normal bg-[#040404] shadow-modalSentForm md:absolute md:left-[50%]  md:top-[50%] md:mt-auto md:w-auto md:translate-x-[-50%]   md:translate-y-[-50%] ${className} `}
+        className={`${s.background} relative  z-40 mt-[100px] rounded-normal bg-body shadow-modalSentForm md:absolute md:left-[50%]  md:top-[50%] md:mt-auto md:w-auto md:translate-x-[-50%]   md:translate-y-[-50%] ${className} `}
       >
         <div className="absolute left-0 top-[60px] h-[178px] w-[100%] bg-grad_1100 blur-[150px] "></div>
         <IconBtn
