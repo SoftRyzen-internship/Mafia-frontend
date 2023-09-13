4eec9583e632978446feb93ab912fbcdee23fa03
Volodymyr-Bezyk <99117313+Volodymyr-Bezyk@users.noreply.github.com>
2023-09-13 16:31:18 +0300

    fix/ navigationRow

--- components/NavigationRowLink/NavigationRowLink.tsx
+++ components/NavigationRowLink/NavigationRowLink.tsx
@@ -13,12 +13,10 @@ export const NavigationRowLink: FC<NavigationRowLinkProps> = ({
   onClick,
 }) => {
   const navLinkClasses = classNames(
-    ' block font-raleway text-base hover:text-primary-light-900 focus:text-primary-light-500 transition-colors duration-300 ',
+    ' inline-block text-base hover:text-primary-light-900 focus:text-primary-light-500 transition-colors duration-300 ',
     {
-      'text-primary-light-500':
+      'after:block after:mt-1 after:h-[2px] after:w-full after:bg-grad_100':
         currentPath === href && position === 'mobile-menu',
-      'text-white-light': currentPath !== href && position === 'mobile-menu',
-      'text-white-light ': position === 'header' || position === 'footer',
       'text-sm md:text-base': position === 'footer',
     },
   );
