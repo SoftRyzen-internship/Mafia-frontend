439aa33a7a22caa13a770a3ff5e55f91a12fa131
Volodymyr Bondarenko <qweeqer2012@gmail.com>
2023-09-13 16:10:23 +0300

    fix-mafia-school-Hero/change styles for images and paragraph/change file and folder replace to view/ fix error in HeaderBtn.txs

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,12 @@ import { CorporateParties } from '@/views/CorporateParties';
 import { Slider } from '@/components/Slider';
 import { SliderSchoolElement } from '@/components/SliderSchoolElement';
 import schoolData from '@/data/schoolSection.json';
+import MafiaSchoolHeroSection from '@/views/MafiaSchoolHeroSection/MafiaSchoolHeroSection';
 
 const Home = () => {
   return (
     <>
+      <MafiaSchoolHeroSection />
       <CorporateParties />
       <div className="container">
         <Slider
