032d1d8d3bcc17701207aa5f5e157316da0f87dc
Mariia Kovalova <mariia_kovalova@ukr.net>
2023-09-14 16:47:54 +0300

    header fix

--- views/Header/Header.tsx
+++ views/Header/Header.tsx
@@ -10,7 +10,7 @@ const { button } = data.header;
 export const Header = () => (
   <header className="fixed z-[2] w-full border-b border-gray-light bg-body py-[6px] md:py-[14px] xl:py-[12px]">
     <div className="container flex items-center justify-between">
-      <nav className="flex items-center gap-[24px] md:gap-[64px] xl:gap-[240px] xxl:gap-[342px]">
+      <nav className="flex items-center gap-[24px] md:gap-[64px] xl:gap-[120px] xxl:gap-[342px]">
         <MobileMenu />
         <Logo href="/" position="header" />
         <NavigationRow position="header" className="notXl:hidden" />
