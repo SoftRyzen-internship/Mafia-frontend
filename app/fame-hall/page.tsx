7a0c7fe95aafdd93331594b094640e92c0b73826
yevhen-petrunkin <novamovaxxi@gmail.com>
2023-09-14 17:30:16 +0300

    add tournaments page

---
+++ app/fame-hall/page.tsx
@@ -0,0 +1,11 @@
+import { HallFameSection } from '@/views/HallFameSection';
+
+const HallFame = () => {
+  return (
+    <div>
+      <HallFameSection />
+    </div>
+  );
+};
+
+export default HallFame;
