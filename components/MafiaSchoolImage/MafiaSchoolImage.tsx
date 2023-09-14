0c994dffe592531206fe166d0d4db7b6a8ed8e23
Olga Birkovska <aiba.aki9@gmail.com>
2023-09-14 12:03:45 +0300

    desktop positioning

--- components/MafiaSchoolImage/MafiaSchoolImage.tsx
+++ components/MafiaSchoolImage/MafiaSchoolImage.tsx
@@ -1,12 +1,21 @@
 import Image from 'next/image';
+import classNames from 'classnames';
+
+import css from './MafiaSchoolImage.module.css';
 
 export const MafiaSchoolImage = ({ image, alt }) => {
+  const imageStyles = classNames(
+    'h-[315px] w-full rounded-normal object-cover object-center',
+    'md:h-[630px] md:w-[288px] md:object-left',
+    'xl:h-[412px] xl:w-[493px] xxl:h-[412px] xxl:w-[716px]',
+    css.image,
+  );
   return (
     <div
       className="mb-[76px] h-[315px] w-full rounded-normal shadow-sm
             md:mb-[96px] md:h-[630px] md:w-[288px]
-            xl:absolute xl:right-8 xl:top-[80px] xl:mb-0 xl:h-[644px] xl:w-[493px]
-            xxl:bottom-[120px] xxl:top-auto xxl:h-[778px] xxl:w-[716px]"
+            xl:h-[412px] xl:w-[493px]
+            xxl:bottom-[120px] xxl:top-auto xxl:h-[412px] xxl:w-[716px]"
     >
       <Image
         src={image}
@@ -15,8 +24,7 @@ export const MafiaSchoolImage = ({ image, alt }) => {
         height={630}
         loading="lazy"
         sizes="(max-width: 767px) 328px, (max-width: 1279px) 288px, (max-width: 1751px) 493px, 716px"
-        className="h-[315px] w-full rounded-normal object-cover object-center
-                md:h-[630px] md:w-[288px] md:object-left xl:h-[644px] xl:w-[493px] xxl:h-[778px] xxl:w-[716px]"
+        className={imageStyles}
       />
     </div>
   );
--- views/MafiaSchool/MafiaSchool.tsx
+++ views/MafiaSchool/MafiaSchool.tsx
@@ -5,6 +5,8 @@ import { Heading } from '@/components/Heading';
 import { Paragraph } from '@/components/Paragraph';
 import { MafiaSchoolImage } from '@/components/MafiaSchoolImage';
 import { LearnList } from '@/components/LearnList';
+import { Slider } from '@/components/Slider';
+import { SliderSchoolElement } from '@/components/SliderSchoolElement';
 
 import data from '@/data/schoolSection.json';
 
@@ -22,7 +24,8 @@ export const MafiaSchool = () => {
   const bgGradient = classNames(
     css.gradient,
     'mb-[40px]',
-    'md:absolute md:top-[280px] md:left-0 md:text-right',
+    'md:mb-0 md:absolute md:top-[280px] md:left-0 md:text-right',
+    'xl:top-[256px]',
   );
 
   return (
@@ -32,8 +35,10 @@ export const MafiaSchool = () => {
           {title}
         </Heading>
 
-        <Paragraph className="mb-6 md:w-[339px]">{intro}</Paragraph>
-        <Paragraph className="mb-[60px] md:mb-[218px] md:w-[339px]">
+        <Paragraph className="mb-6 md:w-[339px] xl:w-[340px]">
+          {intro}
+        </Paragraph>
+        <Paragraph className="mb-[60px] md:mb-[218px] md:w-[339px] xl:mb-[220px] xl:w-[465px]">
           {invite}
         </Paragraph>
 
@@ -45,10 +50,22 @@ export const MafiaSchool = () => {
           </Heading>
         </div>
 
-        <LearnList className="mb-[60px] md:absolute md:right-[84px] md:top-[418px]" />
+        <LearnList className="mb-[60px] md:absolute md:right-[84px] md:top-[418px] xl:right-8 xl:top-[416px]" />
 
-        <Paragraph className="mb-6 md:w-[280px]">{description}</Paragraph>
-        <Paragraph className="mb-[19px] md:w-[280px]">{join}</Paragraph>
+        <Paragraph className="mb-6 md:w-[280px] xl:w-[597px]">
+          {description}
+        </Paragraph>
+        <Paragraph className="mb-[48px] md:w-[280px] xl:w-[597px]">
+          {join}
+        </Paragraph>
+
+        <Slider
+          section="school"
+          navigation
+          pagination
+          element={SliderSchoolElement}
+          data={data.swiperData}
+        />
       </div>
     </Section>
   );
