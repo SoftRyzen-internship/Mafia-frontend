ad9f38763cd3c89e13291a0b261acd9d28c7a0d1
Mariia Kovalova <mariia_kovalova@ukr.net>
2023-09-15 17:10:04 +0300

    notify fix

--- components/FormWithPopUp/FormWithPopUp.tsx
+++ components/FormWithPopUp/FormWithPopUp.tsx
@@ -1,31 +1,47 @@
 'use client';
 
 import { useEffect, useState } from 'react';
-import toast from 'react-hot-toast';
 
 import { Form } from '../Form/Form';
 import { PopUpType } from '@/types';
-import data from '@/data/common.json';
-
-const { onSuccess, onError } = data.onFormSubmitNotify;
+import { Portal } from '../Portal';
+import { ModalSendForm } from '../ModalSendForm';
+import { SuccessWindow } from '../SuccessWindow';
+import { ErrorWindow } from '../ErrorWindow';
 
 export const FormWithPopUp = () => {
   const [popUpType, setPopUpType] = useState<PopUpType>('default');
+  const [showModal, setShowModal] = useState(false);
+
+  const handleToggeModal = () => {
+    setShowModal(prev => !prev);
+    document.body.classList.toggle('overflow-hidden');
+  };
 
   useEffect(() => {
     const checkPopUpType = (popUpType: PopUpType) => {
       switch (popUpType) {
-        case 'default':
-          return;
         case 'success':
-          return toast.success(onSuccess);
+          return handleToggeModal();
         case 'error':
-          return toast.error(onError);
+          return handleToggeModal();
       }
     };
 
     checkPopUpType(popUpType);
   }, [popUpType]);
 
-  return <Form setPopUpType={setPopUpType} />;
+  return (
+    <>
+      <Form setPopUpType={setPopUpType} />
+      {showModal ? (
+        <Portal onModalClose={handleToggeModal}>
+          <ModalSendForm onModalClose={handleToggeModal}>
+            {popUpType === 'success' ? <SuccessWindow /> : null}
+            {popUpType === 'error' ? <ErrorWindow /> : null}
+          </ModalSendForm>
+        </Portal>
+      ) : null}
+    </>
+  );
 };
